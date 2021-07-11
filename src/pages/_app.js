323259dcc5346b88be425560c280f1f7a73756c3
vikrant47 <vikrant.singh1990@yahoo.in>
2021-07-11 13:08:00 +0530

    added custom.scss

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,16 @@
-import { Fragment } from "react";
-import App from "next/app";
-import Head from "next/head";
-import withReduxStore from "../lib/with-redux-store";
-import { Provider } from "react-redux";
-import { ToastProvider } from "react-toast-notifications";
-import { persistStore } from "redux-persist";
-import { PersistGate } from "redux-persist/integration/react";
-import { fetchProducts } from "../redux/actions/productActions";
-import products from "../data/products.json";
-import "../assets/scss/style.scss";
-import Preloader from "../components/Preloader";
+import { Fragment } from 'react';
+import App from 'next/app';
+import Head from 'next/head';
+import { Provider } from 'react-redux';
+import { ToastProvider } from 'react-toast-notifications';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import withReduxStore from '../lib/with-redux-store';
+import { fetchProducts } from '../redux/actions/productActions';
+import products from '../data/products.json';
+import '../assets/scss/style.scss';
+import '../assets/scss/custom.scss';
+import Preloader from '../components/Preloader';
 
 class MyApp extends App {
   constructor(props) {
@@ -22,34 +23,34 @@ class MyApp extends App {
     const { Component, pageProps, reduxStore } = this.props;
 
     return (
-      <Fragment>
+      <>
         <Head>
           <title>Oility | React Next JS Multipurpose eCommerce Template</title>
           <meta
-            name="description"
-            content="Oility is Powerful React Next JS Template For Any eCommerce Website. The template is built for sell Fashion Products, Shoes, Bags, Cosmetics, Clothes, Sunglasses, Furniture, Kids Products, Electronics, Stationery Products and Sporting Goods."
+            name='description'
+            content='Oility is Powerful React Next JS Template For Any eCommerce Website. The template is built for sell Fashion Products, Shoes, Bags, Cosmetics, Clothes, Sunglasses, Furniture, Kids Products, Electronics, Stationery Products and Sporting Goods.'
           />
           <meta
-            name="keywords"
-            content="ecommerce, electronics store, Fashion store, furniture store,  react, next js, clean, minimal, modern, online store, responsive, retail, shopping, ecommerce store"
+            name='keywords'
+            content='ecommerce, electronics store, Fashion store, furniture store,  react, next js, clean, minimal, modern, online store, responsive, retail, shopping, ecommerce store'
           />
           <link
-            href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900&display=swap'
+            rel='stylesheet'
           />
           <link
-            href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap'
+            rel='stylesheet'
           />
         </Head>
-        <ToastProvider placement="bottom-left">
+        <ToastProvider placement='bottom-left'>
           <Provider store={reduxStore}>
-            <PersistGate loading={<Preloader />} persistor={this.persistor}>
+            <PersistGate loading={<Preloader/>} persistor={this.persistor}>
               <Component {...pageProps} />
             </PersistGate>
           </Provider>
         </ToastProvider>
-      </Fragment>
+      </>
     );
   }
 }

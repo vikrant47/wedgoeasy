80f735f02bdbded79a03f6fc7bcea13f504105bb
vikrant47 <vikrant.singh1990@yahoo.in>
2021-07-25 15:44:29 +0530

    account section type added

--- src/modules/cms/enums/section.types.js
+++ src/modules/cms/enums/section.types.js
@@ -1,7 +1,5 @@
 export const CMS_SECTION_TYPES = {
-  account_section_type: () => {
-
-  },
+  account_section_type: () => require('../../../components/cms/Account/Account').default,
   header_section_type: () => require('../../../components/cms/Header/Header').default,
   home_banner_section_type: () => require('../../../components/cms/Banner/HomeBanner').default,
   login_section_type: () => require('../../../components/cms/Login/Login').default,
